0b006629c54f7dfd6bf262676a527062fa69eda0
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2025-05-14 09:29:44 -0500

    merge

--- src/app.js
+++ src/app.js
@@ -5,12 +5,7 @@ const categoryRoutes = require('./categories/category.view');
 const paymentMethodRoutes = require('./paymentMethods/paymentMethod.view');
 const customerRoutes = require('./customers/customer.view');
 const saleRoutes = require('./sales/sale.view');
-<<<<<<< HEAD
 const inventoryRoutes = require('./inventory/inventory.view');  
-=======
-const salesReportRoutes = require('./salesReports/salesreport.view');
-
->>>>>>> e35a71ab337507c5ab581ff1620abaaf547d12dc
 const app = express();
 app.use(express.json());
 
@@ -19,12 +14,6 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/payment-methods', paymentMethodRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/sales', saleRoutes);
-<<<<<<< HEAD
 app.use('/inventory', inventoryRoutes);
 
 module.exports = app;
-=======
-app.use('/api/sales-reports', salesReportRoutes);
-
-module.exports = app;
->>>>>>> e35a71ab337507c5ab581ff1620abaaf547d12dc
---
+++ src/shipments/shipments.model.js
@@ -0,0 +1,9 @@
+class Shipment{
+    constructor(id, origin, destination, weight, status) {
+        this.id = id;
+        this.origin = origin;
+        this.destination = destination;
+        this.weight = weight;
+        this.status = status;
+    }
+}
